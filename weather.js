7b36c99b2fb1594c3eea33c0567932e49d6a38ac
Neil Sabde <neilsabde@gmail.com>
2021-11-26 18:46:27 +0530

    Third Commit

--- weather.js
+++ weather.js
@@ -1,7 +1,8 @@
-let coordinates = {};
+let coords = {};
 
 $(document).ready(function () {
   get_coordinates();
+  get_weather();
 });
 
 function get_coordinates() {
@@ -9,12 +10,32 @@ function get_coordinates() {
   if (searchParams.has("source") && searchParams.has("destination")) {
     let source = searchParams.get("source");
     let destination = searchParams.get("destination");
-    coordinates.source_lat = source.split(";")[0];
-    coordinates.source_lon = source.split(";")[1];
-    coordinates.destination_lat = destination.split(";")[0];
-    coordinates.destination_lon = destination.split(";")[1];
+    coords.source_lat = source.split(";")[0];
+    coords.source_lon = source.split(";")[1];
+    coords.destination_lat = destination.split(";")[0];
+    coords.destination_lon = destination.split(";")[1];
   } else {
     alert("Coordinates not selected!");
     window.history.back();
   }
+}
+
+function get_weather() {
+  $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=${coords.destination_lat}&lon=${coords.destination_lon}&appid=94212e971d0ca977303f8ae892224bbd`,
+    type: "get",
+    success: function (response) {
+      let name = response.name;
+      let weather = response.weather[0].main;
+      $("#scene_container").append(
+        `
+          <a-entity gps-entity-place="latitude: ${steps[i].maneuver.location[1]}; longitude: ${steps[i].maneuver.location[0]};">
+              <a-entity>
+                  <a-text height="50" value="Weather forcast is ${weather} at ${name}"></a-text>
+              </a-entity>
+          </a-entity>
+        `
+      );
+    },
+  });
 }
